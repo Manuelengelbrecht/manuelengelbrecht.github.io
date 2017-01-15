df7f4210214f5717da8c1b3a1ad24c488c4a4e53
Manuelengelbrecht <manuel.paul.engelbrecht@gmail.com>
2017-01-15 14:21:09 +0100

    Add files via upload

--- vendor/jquery/Impressum.js
+++ vendor/jquery/Impressum.js
@@ -1,10 +1,41 @@
-     //MPE: Impressum with click
-$( document ).ready(function() {
-    console.log( "ready!" );
-});
-
+//Show Impressum with anchor
 $(document).ready(function(){
-    $("#ImpressumA").click(function() {
-    $("#Impressum").toggle();
-    });
+        $("#ImpressumA").click(function() {
+        $("#Impressum").toggle();
+        });
+    //Hide Impressum with notice
+    $("#Impressum_notice").click(function() {
+        $("#Impressum").hide();
+        });    
+    // Hide Impressum notice (only shpown when hovering Impressum)
+        $("#Impressum_notice").hide(); // hide the fixed navbar initially
+
+        var topofDiv = $("#Impressum").offset().top; //gets offset of Impressum
+        var height = $("#Impressum").outerHeight(); //gets height of Impressum
+        var winheight = $( window ).height();
+
+        $(window).scroll(function(){
+            if($(window).scrollTop() > (topofDiv + height + winheight)){
+               $("#Impressum_notice").show();
+            }
+            else{
+               $("#Impressum_notice").hide();
+            }
+        });
+    //Hide footer & Name when scrolling
+
+        var topofDiv2 = $("#portfolio").offset().top; //gets offset of contact section
+        var height2 = $("#footer").outerHeight(); //gets height of section
+
+        $(window).scroll(function(){
+            if($(window).scrollTop() > (topofDiv2 - 2*height2)){
+               $("#Footer_fix").fadeOut("fast");
+                $(".DoubleHeader").fadeOut("fast");
+            }
+            else{
+               $("#Footer_fix").show();
+                $(".DoubleHeader").show();
+            }
+        });
+        
 });
